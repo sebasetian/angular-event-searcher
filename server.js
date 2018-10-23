3b8839f8c8e0da3cbb586dd793325bd5078a780f
shih-yao <sebasetian123@gmail.com>
2018-10-23 10:38:21 -0700

    change the port setting

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = 8081;
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/scripts'));
+app.set('port', process.env.PORT || port);
 app.get('/', (req, res) => 
 	res.sendFile('/index.html')
 );
