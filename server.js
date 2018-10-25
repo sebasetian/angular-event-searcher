002cc4436266803e1eba98776e195f613a66a53b
shih-yao <sebasetian123@gmail.com>
2018-10-24 20:48:35 -0700

    add angular 7

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const http = require('http');
 const port = 8081;
-app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/scripts'));
+// Point static path to dist
+app.use(express.static(__dirname + '/dist/hw8'));
 app.set('port', process.env.PORT || port);
 app.get('/', (req, res) => 
-	res.sendFile('/index.html')
+	res.sendFile('index.html')
 );
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// "https://maps.googleapis.com/maps/api/js?key=AIzaSyAHzFtoldQOPyMVNEEJZN8QE5Adj-SuW0Q"
+// "https://app.ticketmaster.com/discovery/v2/events.json?apikey=faDkniVNhw8P88x5ljxBPGxEUbtD5Ulb&keyword=" 
+app.listen(port, () => console.log(`Example app listening on port ${process.env.PORT || port}!`));
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'hw8';
+}
